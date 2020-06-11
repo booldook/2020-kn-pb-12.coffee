a2d1d6f19e3cdc88ffa01d77aed2dc2c2c117e08
booldook <booldook@gmail.com>
2020-06-11 13:08:53 +0900

    slide 짜는중

--- js/index.js
+++ js/index.js
@@ -18,8 +18,23 @@ mainSlide();
 
 /******************* 사용자 함수 ********************/
 function mainSlide() {
-	var $slide = $(".main-wrap").find(".banner").remove();
-	console.log($slide);
+	var now = 0;
+	var $slide = $(".main-wrap").find(".banner").css("transition", "0.5s").remove();
+	// console.log($slide);
+	var last = $slide.length - 1;
+
+	function init() {
+		$($slide[now]).appendTo('.main-wrap');
+		now = (now == 2) ? now = 0 : now + 1;
+		$($slide[now]).appendTo('.main-wrap').css({"opacity": 0, "transform": "scale(1.2)"});
+	}
+	function onClick() {
+		$(".banner").eq(0).css({"opacity": 0, "transform": "scale(0.7)"});
+		$(".banner").eq(1).css({"opacity": 1, "transform": "scale(1)"});
+		setTimeout(init, 500);
+	}
+	$(".main-wrap").click(onClick);
+	init();
 }
 
 
