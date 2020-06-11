5511fc25798641fe7a0c00c14e77301484aee5c0
booldook <booldook@gmail.com>
2020-06-11 11:21:39 +0900

    header scroll

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/******************* 사전지식 ********************/
+
+
+/******************* 전역설정 ********************/
+
+
+
+/******************* 사용자 함수 ********************/
+
+
+/******************* 이벤트 함수 ********************/
+function onResize() {
+	this.wid = $(this).innerWidth();
+	this.hei = $(this).innerHeight();
+}
+
+function onScroll() {
+	this.scTop = $(this).scrollTop();
+	if(this.scTop > this.hei) {
+		$(".header").css({"top": 0, "bottom": "auto", "position": "fixed"});
+	}
+	else {
+		$(".header").css({"top": "auto", "bottom": 0, "position": "absolute"});
+	}
+}
+
+/******************* 이벤트 설정 ********************/
+$(window).resize(onResize).trigger("resize");
+$(window).scroll(onScroll).trigger("scroll");
