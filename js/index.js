bddef98ede37dbf27da7676d9f73cfe9ae5309d6
booldook <booldook@gmail.com>
2020-06-12 12:02:53 +0900

    slide 객체화

--- js/index.js
+++ js/index.js
@@ -18,8 +18,8 @@ $btRight.css("right", "2rem");
 */
 
 /******************* 전역설정 ********************/
-mainSlide(".main-wrap", ".banner", onComplete);
-
+Slide.scale(".main-wrap", ".banner", onComplete);
+Slide.scale(".main-wrap2", ".banner", onComplete);
 
 
 /******************* 사용자 함수 ********************/
@@ -28,42 +28,6 @@ function onComplete(prevSlide, nextSlide, container) {
 	$(prevSlide).find(".writer").css({"opacity": 0, "transform": "translateY(5vw)"});
 	$(nextSlide).find(".slogan").css({"opacity": 1, "transform": "scale(1)"});
 	$(nextSlide).find(".writer").css({"opacity": 1, "transform": "translateY(0)"});
-}
-
-function mainSlide(container, slide, cb) {
-	var now = 0;
-	var $container = $(container).addClass("slide-wrap");
-	var $slide = $(slide).addClass("slide");
-	var $btPrev = $('<div class="bt bt-prev"></div>').appendTo($container).click(onPrev);
-	var $btNext = $('<div class="bt bt-next"></div>').appendTo($container).click(onNext);
-	var last = $slide.length - 1;
-
-	function init() {
-		$btPrev.show();
-		$btNext.show();
-		$container.children(".slide").remove();
-		$($slide[now]).appendTo($container);
-	}
-	function ani() {
-		$($slide[now]).appendTo($container).css({"opacity": 0, "transform": "scale(1.2)"});
-		setTimeout(function(){
-			$container.find(".slide").eq(0).css({"opacity": 0, "transform": "scale(0.7)"});
-			$container.find(".slide").eq(1).css({"opacity": 1, "transform": "scale(1)"});
-			cb($container.find(".slide").eq(0), $container.find(".slide").eq(1), $container);	
-			setTimeout(init, 500);
-		}, 0);
-	}
-	function onPrev() {
-		$(this).hide();
-		now = (now == 0) ? last : now - 1;
-		ani();
-	}
-	function onNext() {
-		$(this).hide();
-		now = (now == 2) ? 0 : now + 1;
-		ani();
-	}
-	init();
 }
 
 
--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,7 @@
 reference
 - type: hori, vert, fade, scale, prd : 슬라이드 타입
 - container : 슬라이드들이 담길 객체
-*/
+
 var Slide = (function(){
 	function Slide(obj) {
 		this.obj = obj || {};
@@ -11,8 +11,6 @@ var Slide = (function(){
 	}
 	return Slide;
 })();
-
-
 
 var mainSlide = new Slide({
 	type: 'scale', 
@@ -26,3 +24,56 @@ var mainSlide = new Slide({
 	}
 });
 console.log(mainSlide);
+*/
+
+var Slide = (function(){
+	function Slide(container, slide, type, cb) {
+		this.container = $(container);
+		this.slide = $(container).find(slide);
+		if(typeof type == "String") this.type = type;
+		else if(typeof type == "function") this.cb = type;
+		this.cb = cb;
+	}
+	return Slide;
+})();
+
+
+
+var Slidessssss = {
+	scale: function(container, slide, cb) {
+		var now = 0;
+		var $container = $(container).addClass("slide-wrap");
+		console.log($container);
+		var $slide = $(slide).addClass("slide");
+		var $btPrev = $('<div class="bt bt-prev"></div>').appendTo($container).click(onPrev);
+		var $btNext = $('<div class="bt bt-next"></div>').appendTo($container).click(onNext);
+		var last = $slide.length - 1;
+	
+		function init() {
+			$btPrev.show();
+			$btNext.show();
+			$container.children(".slide").remove();
+			$($slide[now]).appendTo($container);
+		}
+		function ani() {
+			$($slide[now]).appendTo($container).css({"opacity": 0, "transform": "scale(1.2)"});
+			setTimeout(function(){
+				$container.find(".slide").eq(0).css({"opacity": 0, "transform": "scale(0.7)"});
+				$container.find(".slide").eq(1).css({"opacity": 1, "transform": "scale(1)"});
+				cb($container.find(".slide").eq(0), $container.find(".slide").eq(1), $container);	
+				setTimeout(init, 500);
+			}, 0);
+		}
+		function onPrev() {
+			$(this).hide();
+			now = (now == 0) ? last : now - 1;
+			ani();
+		}
+		function onNext() {
+			$(this).hide();
+			now = (now == 2) ? 0 : now + 1;
+			ani();
+		}
+		init();
+	}
+};
